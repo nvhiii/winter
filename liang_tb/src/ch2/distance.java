7de0db87a1f8b36c4fce5a4b439b9ea35b601e36
nvhiii <nahikhan16@gmail.com>
2023-12-29 13:03:57 -0500

    Did a 2.8 example as a refresher

---
+++ liang_tb/src/ch2/distance.java
@@ -0,0 +1,16 @@
+package ch2;
+
+public class distance {
+
+    public static void main(String[] args) {
+
+        double miles = 100.0;
+        final double KILOMETERS_PER_MILE = 1.609;
+
+        double kilometers = miles * KILOMETERS_PER_MILE;
+
+        System.out.println(kilometers);
+
+    }
+
+}
