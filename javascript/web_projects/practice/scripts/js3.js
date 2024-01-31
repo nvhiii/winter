1599357f72c1d5f91bfa4284187571e9a0fcd142
nvhiii <nahikhan16@gmail.com>
2024-01-31 12:50:59 -0500

    now up to numbers portion of js foundations in odins proj, continuing later

---
+++ javascript/web_projects/practice/scripts/js3.js
@@ -0,0 +1,6 @@
+let ourPlanet = "Earth"; // can be const bc our planet never changes
+
+let currentVisitor;
+
+// if we know a variable value before runtime, meaning no function calls needed, we can make that cosnt variable uppercase
+// on the contrary, if we do not, then keep it lowercase
