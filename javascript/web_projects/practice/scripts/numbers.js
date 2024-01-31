ce10226ebc399a638467931329537c24cd108567
nvhiii <nahikhan16@gmail.com>
2024-01-31 18:43:04 -0500

    did some more numbers practice in js, will need to review bc of tangent

---
+++ javascript/web_projects/practice/scripts/numbers.js
@@ -0,0 +1,26 @@
+let num1 = prompt("First number?");
+let num2 = prompt("Second number?");
+
+console.log(+num1 + +num2);
+
+let sum = 1 + 2 + 3 + 4 + 5 + 6;
+console.log(sum);
+
+let quotient = (4 + 6 + 9) / 77;
+console.log(quotient.toFixed(5));
+
+let a = 10;
+console.log(a);
+console.log((9*a))
+
+let b = 7 * a;
+console.log(b);
+
+const MAX = 57;
+let actual = MAX - 13;
+
+let percentage = actual / MAX;
+console.log(percentage.toFixed(5));
+
+//print to alert window
+//alert(+num1 + +num2);
