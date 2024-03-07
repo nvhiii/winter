7b6835c18bc5126892bf35f129b95463ece30b2c
nvhiii <nahikhan16@gmail.com>
2024-03-06 21:13:33 -0500

    created form basics using js, interesting

--- javascript/web_projects/practice/scripts/dom-practice.js
+++ javascript/web_projects/practice/scripts/dom-practice.js
@@ -26,10 +26,37 @@ myDiv2.appendChild(myP);
 myDiv.appendChild(myDiv2);
 
 // create const to hold button and the appropriate response via nodelist (can swap to array if needed)
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("div1");
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    alert(`You pressed button ${button.id}`);
+// buttons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     alert(`You pressed button ${button.id}`);
+//   });
+// });
+
+const inputBar = document.querySelector("#item");
+const inputButton = document.querySelector("button");
+const outputList = document.querySelector("ul");
+
+inputButton.addEventListener("click", () => {
+  let val = inputBar.value;
+  inputBar.value = ``;
+
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const itemButton = document.createElement("button");
+
+  li.appendChild(span);
+  li.appendChild(itemButton);
+
+  span.textContent = val;
+  itemButton.textContent = "Delete";
+
+  outputList.appendChild(li);
+
+  itemButton.addEventListener("click", () => {
+    outputList.removeChild(li);
   });
+
+  inputBar.focus();
 });
